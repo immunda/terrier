c9fe2235c1defd6747fc4c46e89bfd1a2ce89c1c
Phil Howell <phil@quae.co.uk>
2016-08-16 09:52:40 +0100

    Switch to setuptools, additional setup metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="terrier",
+    author="Phil Howell",
+    author_email="phil@quae.co.uk",
     version="0.1dev",
-    packages=["click",],
+    url="https://github.com/immunda/terrier",
+    install_requires=["click",],
+    pymodules=["terrier"],
+    entry_points="""
+        [console_scripts]
+        terrier=terrier:cli
+    """,
     license="MIT",
     long_description=open("README.md").read(),
 )
